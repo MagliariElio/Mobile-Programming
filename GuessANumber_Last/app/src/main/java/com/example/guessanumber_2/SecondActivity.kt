dcdffbebda0ec3e8a35f133946447b8e0ad36ddb
Mike-98 <micheletosi98@gmail.com>
2021-04-06 19:39:08 +0200

    Last Version

---
+++ GuessANumber_Last/app/src/main/java/com/example/guessanumber_2/SecondActivity.kt
@@ -0,0 +1,27 @@
+package com.example.guessanumber_2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import com.example.guessanumber_2.databinding.ActivityMainBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imgBtnSecondActivity = findViewById<ImageButton>(R.id.ib_Hello)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = ""
+
+        imgBtnSecondActivity.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
