ffdc46547eee1b5836cafed1186e31aab5f973f5
Mike-98 <micheletosi98@gmail.com>
2021-05-08 12:33:17 +0200

    rimosso massimo regoli

--- CodiceFiscale/app/src/main/java/it/massimoregoli/codicefiscale/viewmodels/CountryModel.kt
+++
@@ -1,9 +0,0 @@
-package it.massimoregoli.codicefiscale.viewmodels
-
-data class CountryModel(var description: String,
-                        var province: String,
-                        var code: String) {
-    override fun toString(): String {
-        return  description
-    }
-}
