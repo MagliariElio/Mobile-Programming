d5ec0889010b5eed9fa57f4888e1bc807dead50e
Mike-98 <micheletosi98@gmail.com>
2021-05-08 13:16:51 +0200

    Codice Fiscale rimosso

--- CodiceFiscale/app/src/main/java/it/mem/codiceFiscale/viewmodels/CountryModel.kt
+++
@@ -1,9 +0,0 @@
-package it.mem.codiceFiscale.viewmodels
-
-data class CountryModel(var description: String,
-                        var province: String,
-                        var code: String) {
-    override fun toString(): String {
-        return  description
-    }
-}
